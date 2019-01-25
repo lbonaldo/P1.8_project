939246155553928f4fd73fa1a1dd32928426a80f
Lagrang3 <lagrangelciencia@gmail.com>
2019-01-25 12:16:56 +0100

    Check the units of measure.

--- ljmd-c/include/utilities.h
+++ ljmd-c/include/utilities.h
@@ -4,8 +4,31 @@
 #define UTILITIES_H
 
 /* a few physical constants */
-static const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
+static const double Na=6.022140857e23; /* Avogadro's constant, number of atoms in a mol  */
+static const double jtokcal=0.00023900573614; /* 1 J/kcal  */
+static const double kb_SI=1.38064852e-23; /* Boltzmann constant in international units J/K  */
+static const double amu_kg=1.660539040e-27; /* 1 atomic mass in kg  */
+static const double A_m=1e-10; /* 1 armstrong in m */
+
+static const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K = kb_SI*Na*jtokcal */
+/*
+	m*v^2 	= m_{kg} * v_{m/s}^2 * kg * (m/s)^2
+			= m_{kg} * v_{m/s}^2 * J
+			= m_{amu} * v_{m/s}^2 * amu/kg * J
+			= m_{amu} * v_{m/s}^2 * amu/kg * J/kcal * kcal
+			= m_{amu} * v_{m/s}^2 * amu/kg * J/kcal * Na * kcal/mol
+			= m_{amu} * v_{m/s}^2 * 2.390057e-7 * kcal/mol
+			= m_{amu} * v_{A/T}^2 * ( (A/T)/(m/s) )^2 * 2.390057e-7 * kcal/mol
+			= m_{amu} * v_{A/T}^2 * (s/T * 1e5 )^2 * 2390.057 * kcal/mol
+			
+	where A is armstrong and T is some unknown unit of time;
+	then if Axel insists that mvsq2e=2390.057
+	we must have that the velocities are given in units of A/T
+	where T = 1e-5 seconds.
+*/
 static const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
+
+
 /* helper function: zero out an array */
 void azzero(double *d, const int n);
 
