49514374162842af0174465e457214c42fecb66b
Lagrang3 <lagrangelciencia@gmail.com>
2019-01-26 18:08:40 +0100

    Tests measure speedup.

--- ljmd-c/test/run_test.sh
+++ ljmd-c/test/run_test.sh
@@ -31,6 +31,16 @@ fi
 echo " Accepted."
 
 echo ""
+export A=$( { /usr/bin/time -f "%e" echo "1" ; } 2>&1 >/dev/null ) 
+t1=$( { /usr/bin/time -f "%e" ../ljmd-serial.x < ../examples/argon_108.inp ; } 2>&1 >/dev/null ) 
+t2=$( { /usr/bin/time -f "%e" ../ljmd-split.x < ../examples/argon_108.inp ; } 2>&1 > /dev/null)
+sup=$(python -c "print('{0:.2f}'.format($t1/$t2))")
+
+echo "Time for serial: $t1 seconds"
+echo "Time for split: $t2 seconds"
+echo "Speed up x$sup"
+
+echo ""
 echo "--------------- ALL TESTS PASSED ------------------"
 echo "---------------------------------------------------"
 
