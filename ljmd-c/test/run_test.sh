959f7536a8031f224cc98a2e7bc53bca3dfb67e7
Lagrang3 <lagrangelciencia@gmail.com>
2019-01-26 21:09:13 +0100

    Added a time test for 2000 atoms

--- ljmd-c/test/run_test.sh
+++ ljmd-c/test/run_test.sh
@@ -32,15 +32,26 @@ fi
 echo " Accepted."
 
 echo ""
+echo " ... argon_108 ..."
 export A=$( { /usr/bin/time -f "%e" echo "1" ; } 2>&1 >/dev/null ) 
-t1=$( { /usr/bin/time -f "%e" ../ljmd-serial.x < ../examples/argon_108.inp ; } 2>&1 >/dev/null ) 
-t2=$( { /usr/bin/time -f "%e" ../ljmd-split.x < ../examples/argon_108.inp ; } 2>&1 > /dev/null)
+t1=$( { /usr/bin/time -f "%e" ../ljmd-serial.x < ./argon_108.inp ; } 2>&1 >/dev/null ) 
+t2=$( { /usr/bin/time -f "%e" ../ljmd-split.x < ./argon_108.inp ; } 2>&1 > /dev/null)
 sup=$(python -c "print('{0:.2f}'.format($t1/$t2))")
 
 echo "Time for serial: $t1 seconds"
 echo "Time for split: $t2 seconds"
 echo "Speed up x$sup"
 
+echo ""
+echo " ... argon_2916 ..."
+export A=$( { /usr/bin/time -f "%e" echo "1" ; } 2>&1 >/dev/null ) 
+t1=$( { /usr/bin/time -f "%e" ../ljmd-serial.x < ./argon_2916.inp ; } 2>&1 >/dev/null ) 
+t2=$( { /usr/bin/time -f "%e" ../ljmd-split.x < ./argon_2916.inp ; } 2>&1 > /dev/null)
+sup=$(python -c "print('{0:.2f}'.format($t1/$t2))")
+
+echo "Time for serial: $t1 seconds"
+echo "Time for split: $t2 seconds"
+echo "Speed up x$sup"
 echo ""
 echo "--------------- ALL TESTS PASSED ------------------"
 echo "---------------------------------------------------"
