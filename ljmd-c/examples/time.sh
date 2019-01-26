0fc562103a489497a14ee788b65d8b87cb755970
Lagrang3 <lagrangelciencia@gmail.com>
2019-01-26 23:06:02 +0100

    Tunning of parameters for testing time.
    modified:   ljmd-c/Obj-split/Makefile
    I removed the flag '-g', since that one is used for debugging
    and probably you don't get the best optimization.
    
    modified:   ljmd-c/examples/argon_2916.inp
    I reduced the frequency of print operations since we are interested
    in computation times. Now we print one in every 1000 steps.
    
    modified:   ljmd-c/examples/time.sh
    I added the explicit declaration of omp threads and
    used the '-f "%e"' option of /usr/bin/time in order to
    retrieve the wall time, not the user time.

--- ljmd-c/examples/time.sh
+++ ljmd-c/examples/time.sh
@@ -4,9 +4,10 @@
 
 for N in 108 2916
 do
-    /usr/bin/time ../ljmd-split.x < argon_$N.inp 2> tmp.dat
+	export OMP_NUM_THREADS=4
+    /usr/bin/time -f "%e" ../ljmd-split.x < argon_$N.inp 2> tmp.dat
     cat tmp.dat | head -n 1 | cut -d ' ' -f1 1> tmp2.dat
-    awk -v N=$N '{print N " Step 1: " $1}' tmp2.dat 1>> ../test/time.txt
+    awk -v N=$N '{print N " Step 2: " $1}' tmp2.dat 1>> ../test/time.txt
 
 rm tmp.dat
 rm tmp2.dat
