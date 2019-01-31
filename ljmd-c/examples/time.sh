da64ccd9545ebcedddb9eaa6d4adf32466c49a26
costantino pacilio <costantinopacilio7@hotmail.it>
2019-01-31 23:15:17 +0100

    time measured

--- ljmd-c/examples/time.sh
+++ ljmd-c/examples/time.sh
@@ -4,7 +4,7 @@
 
 for N in 2916
 do
-	export OMP_NUM_THREADS=4
+    export OMP_NUM_THREADS=4
     /usr/bin/time -f "%e" ../ljmd-split.x < argon_$N.inp 2> tmp.dat
     cat tmp.dat | head -n 1 | cut -d ' ' -f1 1> tmp2.dat
     awk -v N=$N '{print N " Step 1-cell: " $1}' tmp2.dat 1>> ../test/time.txt
